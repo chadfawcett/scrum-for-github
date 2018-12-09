1bbc46ccd31916d50e7cea9e0dbd78721a10b2eb
Chad Fawcett <me@chadf.ca>
2018-12-09 12:55:05 -0800

    Get issue estimate from title

--- projects.js
+++ projects.js
@@ -1,3 +1,7 @@
+//  Point estimation syntax
+//  Match a positive number (integer or decimal) wrapped in square brackets
+const estimatePattern = /\s?\[(?<estimateValue>(?:0|[1-9]\d*)?(?:\.\d+)?)\]\s?/
+
 function ensureIssueCardLabelsExist(issueCard) {
   let labels = issueCard.querySelector('.labels')
   if (!labels) {
@@ -38,47 +42,90 @@ function ensureColumnPointsLabelExists(column) {
   return pointsLabel
 }
 
-function updateIssueCardPoints(issueCard, issuePoints) {
-  const pointsLabel = ensureIssueCardLabelsExist(issueCard)
-  pointsLabel.innerText = issuePoints
-}
-
-function updateColumnPoints(column, columnPoints) {
-  const pointsLabel = ensureColumnPointsLabelExists(column)
-  pointsLabel.innerText = columnPoints
-}
-
-const columns = document.querySelectorAll('.project-column')
-
 const columnObserver = new MutationObserver(mutations => {
   mutations.forEach(async ({ addedNodes, target: column }) => {
     if (addedNodes.length) {
       //  Note these are organized by the actual issue ids and not issue-card ids
       const issueCards = Array.from(column.querySelectorAll('.issue-card'))
-      const issueIds = issueCards.map(i => i.dataset.contentId)
-      const issuePointsById = await browser.storage.sync.get(issueIds)
 
       //  Update each card points value, and calculate column total
       const columnPoints = issueCards
         .map(issueCard => {
-          const issuePoints = issuePointsById[issueCard.dataset.contentId]
+          const issueTitle = issueCard.querySelector(
+            'a.js-project-card-issue-link'
+          )
+          if (!issueTitle) return 0
 
-          if (!issuePoints) return 0
+          const issuePointsMatch = estimatePattern.exec(issueTitle.innerText)
+          if (!issuePointsMatch) return 0
 
-          updateIssueCardPoints(issueCard, issuePoints)
+          const [estimateMatch, estimateValue] = issuePointsMatch
 
-          return issuePoints
+          //  Update label value
+          const pointsLabel = ensureIssueCardLabelsExist(issueCard)
+          pointsLabel.innerText = estimateValue
+
+          //  Remove estimate from issue title
+          issueTitle.innerText = issueTitle.innerText
+            .replace(estimateMatch, ' ')
+            .trim()
+
+          return parseFloat(estimateValue)
         })
         .reduce((acc, val) => acc + val)
 
-      updateColumnPoints(column, columnPoints)
+      //  Update column points
+      const pointsLabel = ensureColumnPointsLabelExists(column)
+      pointsLabel.innerText = columnPoints
     }
   })
 })
 
+const columns = document.querySelectorAll('.project-column')
 columns.forEach(column =>
   columnObserver.observe(
     document.getElementById(`column-cards-${column.dataset.id}`),
     { childList: true }
   )
 )
+
+function ensureIssueDetailsPaneLabelExists(issueDetailsPane) {
+  const issueHeader = issueDetailsPane.querySelector('a > span.js-issue-title')
+
+  let pointsLabel = issueHeader.querySelector('.points-label')
+  if (!pointsLabel) {
+    pointsLabel = document.createElement('button')
+    pointsLabel.className =
+      'points-label points-label--title js-card-filter issue-card-label ' +
+      'IssueLabel v-align-middle css-truncate css-truncate-target'
+    issueHeader.parentNode.insertBefore(pointsLabel, issueHeader)
+  }
+
+  return pointsLabel
+}
+
+const issueDetailsPaneObserver = new MutationObserver(mutations => {
+  console.log(mutations)
+  const { target: issueDetailsPane } = mutations[mutations.length - 1]
+
+  const issueTitle = issueDetailsPane.querySelector('a > span.js-issue-title')
+  if (!issueTitle) return
+
+  const issuePointsMatch = estimatePattern.exec(issueTitle.innerText)
+  if (!issuePointsMatch) return
+
+  const [estimateMatch, estimateValue] = issuePointsMatch
+
+  //  Update label value
+  const pointsLabel = ensureIssueDetailsPaneLabelExists(issueDetailsPane)
+  pointsLabel.innerText = estimateValue
+
+  //  Remove estimate from issue title
+  issueTitle.innerText = issueTitle.innerText.replace(estimateMatch, ' ').trim()
+})
+
+const issueDetailsPane = document.querySelector('.js-project-card-details')
+issueDetailsPaneObserver.observe(issueDetailsPane, {
+  childList: true,
+  subtree: false
+})
